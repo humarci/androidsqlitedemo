f2286baf4a4c56dc52a72d4b8381cc9e8df9ba4b
Márton Huszics <MHUSZICS@hu.ibm.com>
2020-11-13 10:05:17 +0100

    Update MainActivity.java

--- app/src/main/java/hu/martonhuszics/databasedemo/MainActivity.java
+++ app/src/main/java/hu/martonhuszics/databasedemo/MainActivity.java
@@ -1,14 +1,19 @@
 package hu.martonhuszics.databasedemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.security.Key;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,11 +39,44 @@ public class MainActivity extends AppCompatActivity {
                 Bundle datatBundle = new Bundle();
                 datatBundle.putInt("id", id_to_search);
 
-                //TODO
-                //Intent intent = new Intent(getApplicationContext(), DisplayContact.class)
-                //intent().putExtras(datatBundle);
-                //startActivity(intent);
+                Intent intent = new Intent(getApplicationContext(), DisplayContact.class);
+                intent.putExtras(datatBundle);
+                startActivity(intent);
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the meni; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Bundle bundle = new Bundle();
+                bundle.putInt("id", 0);
+
+                Intent intent = new Intent(getApplicationContext(), DisplayContact.class);
+                intent.putExtras(bundle);
+
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public boolean onKeyDown(int keycode, KeyEvent event) {
+        if (keycode == KeyEvent.KEYCODE_BACK) {
+            moveTaskToBack(true);
+        }
+        return super.onKeyDown(keycode, event);
+    }
+
 }
